f830bd0188c78d6b68ca235b49958d064fbec89b
Chris Lawlor <lawlor.chris@gmail.com>
2013-04-11 13:27:00 -0400

    Minor template fixes, default home page to index.html

--- apps/project/urls.py
+++ apps/project/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
 
 from django.contrib import admin
 admin.autodiscover()
@@ -6,4 +7,5 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'accounts/', include('django.contrib.auth.urls')),
+    url(r'^$', TemplateView.as_view(template_name='index.html'), name='home'),
 )
