96ffda65a3e647375dace38939576cffbbef41f4
Chris Lawlor <lawlor.chris@gmail.com>
2013-07-16 07:47:11 -0400

    Adds staticfiles finder for django compressor
    Fixes server 500 error when COMPRESS=True

--- apps/project/settings/base.py
+++ apps/project/settings/base.py
@@ -82,6 +82,7 @@ STATICFILES_DIRS = (
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'compressor.finders.CompressorFinder',
     #'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
